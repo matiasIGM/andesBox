2a4b58b6d8f0db80432b2e398ed8591d1f07d52f
Matias Garces Martinez <54512479+matiasIGM@users.noreply.github.com>
2023-06-07 16:39:52 -0400

    mejora arreglo movimientos modelo envio

--- andesRestApi/serializers.py
+++ andesRestApi/serializers.py
@@ -5,6 +5,12 @@ class MovimientoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movimiento
         fields = '__all__'
+        
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation.pop('id', None)
+        representation.pop('envio', None)
+        return representation    
 
 class EnvioSerializer(serializers.ModelSerializer):
     movimientos = MovimientoSerializer(many=True, read_only=True)
