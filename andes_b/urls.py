57b46ae24132087ec2d1f1c3762ade4f1b3e0d3c
Matias Garces Martinez <54512479+matiasIGM@users.noreply.github.com>
2023-06-08 18:16:01 -0400

    configuración de front básico para interactuar con la api

--- andes_b/urls.py
+++ andes_b/urls.py
@@ -23,7 +23,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
+from django.views.generic import TemplateView
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -55,4 +55,7 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('obtener-datos/', obtener_datos, name='obtener-datos'),
+
+    #URLS FRONT
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
